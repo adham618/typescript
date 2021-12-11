58486a65bf7cd96e08fb9a2ebbe535a62b28b09a
adham618 <adhamtarek291@gmail.com>
2021-12-11 21:09:27 +0200

    module structure

--- public/app.js
+++ public/app.js
@@ -1,22 +1,4 @@
-"use strict";
-class Invoice {
-    // client: string
-    // details: string
-    // amount: number
-    constructor(
-    // c: string, d: string, a: number
-    client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-        // this.client = c
-        // this.details = d
-        // this.amount = a
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/invoice.js";
 const inVone = new Invoice("adam", "works on the adams website", 250);
 let invoices = [];
 invoices.push(inVone);
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,4 @@
-class Invoice{
-  // client: string
-  // details: string
-  // amount: number
-
-  constructor(
-    // c: string, d: string, a: number
-    readonly client: string,
-    private details: string,
-    public amount: number,
-    
-  ) {
-    // this.client = c
-    // this.details = d
-    // this.amount = a
-  }
-  format() {
-    return `${this.client} owes $${this.amount} for ${this.details}`
-  }
-}
+import {Invoice} from "./classes/invoice.js"
 
 const inVone = new Invoice("adam", "works on the adams website", 250)
 
